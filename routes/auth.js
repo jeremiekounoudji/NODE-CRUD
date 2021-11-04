3bbc6e57db9cd625d32276bb34f1d8194233d1c1
jeremiekounoudji <72901257+jeremiekounoudji@users.noreply.github.com>
2021-11-04 19:28:35 +0100

    more operation

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,18 @@
 const router = require('express').Router();
-const User = require('../models/userModel')
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt')
 
 
 //auth routes
 router.post('/register', async (req,res) =>{
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password,salt)
     // creer un user
     const user = new User({
         username : req.body.username,
         email : req.body.email,
-        password : req.body.password,
+        password : hashedPass,
 
     });
 
@@ -26,7 +30,17 @@ router.post('/register', async (req,res) =>{
 router.get('/all', async (req,res) =>{
     
     try {
-        const users = await User.find().select();
+        const users = await User.find().select("-password");
+        res.status(200).json(users)
+    } catch (error) {
+        res.send('unable to get users')
+    }
+    
+}),
+router.get('/search', async (req,res) =>{
+    let {username,} = req.query;
+    try {
+        const users = await User.find().select("-password").where("username").equals(username);
         res.status(200).json(users)
     } catch (error) {
         res.send('unable to get users')
@@ -34,28 +48,74 @@ router.get('/all', async (req,res) =>{
     
 }),
 
-router.get('/single/:id', async (req,res) =>{
-    const id = req.params._id;
+router.get('/single/:id',  (req,res) =>{
+    const id = req.params.id;
+
     // try {
-        await User.findById(id, (err,docs)=>{
+        
+         User.findOne({_id:id}, (err,docs)=>{
             if (!err) {
                 console.log('loooog1111');
-                res.json().send(docs)
+                res.send(docs)
                 
             } else {
                 console.log('loooog2222');
 
-                res.status(400).send('Id unknown')
+                res.status(400).send('Id unknown' + id)
 
             }
         });
-    //     // res.json(singleUser);
-    // } catch (err) {
-    //     console.log('loooog3333');
-
-    //     res.status(400).send(err)
-
-    // }
+   
     
 }),
+
+router.put("update/:id", async (req,res) =>{
+
+    // try {
+    console.log('loooog1111');
+
+        await User.findByIdAndUpdate(
+            
+            req.params.id,
+            {
+               
+               $push: {username:req.body.username,
+                email:req.body.email,
+                password:req.body.password,
+               }
+                
+                
+            },
+            {
+                new:true,
+                upsert:true,
+                setDefaultsOnInsert:true
+            },
+            (err, doc)=>{
+                console.log('loooog11((((');
+
+                if (!err) {
+                    res.status(200).send(doc)
+                }
+                res.status(400).json(err)
+
+            }
+
+        );
+        
+
+    // } catch (err) {
+    // console.log('looo(--11');
+
+    //     res.status(400).json(err)
+    // }
+}),
+
+router.delete("/delete/:id",async (req, res)=>{
+    const id = req.params.id;
+    await User.findOneAndDelete(id, (err,doc)=>{
+        if(!err) res.status(200).json({"message":"deleted"});
+        else res.send(err)
+    })
+})
 module.exports = router;
